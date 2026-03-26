4f7a58eb10b102cfd401656e2d41c28b1ab5bba5
naama_code <naanas344@gmail.com>
2026-03-26 15:49:56 +0200

    add Note model

---
+++ src/models/note.ts
@@ -0,0 +1,15 @@
+
+
+export interface Note {
+    id: string,
+    title: string,
+    body: string,
+    createdAt: number,
+    userId: string,
+    imageUrl:string
+    location: {
+        latitude: number;
+        longitude: number;
+    };
+    
+}
